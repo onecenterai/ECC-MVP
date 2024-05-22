acbe132a8bef3310552ba800083120fbb132ba7b
Gift-py <giftabumere247@gmail.com>
2024-05-22 15:46:39 +0100

    .

---
+++ logger.py
@@ -0,0 +1,11 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+info_handler = logging.FileHandler('/sw_log.log')
+info_handler.setLevel(logging.INFO)
+
+info_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+info_handler.setFormatter(info_format)
+
+log.addHandler(info_handler)
--- sw.py
+++ sw.py
@@ -4,7 +4,7 @@ import os
 from app.call.model import Call
 from helpers.langchain import qa_chain
 from app import app
-
+from logger import log
 
 class CustomConsumer(Consumer):
     def setup(self):
@@ -17,18 +17,21 @@ class CustomConsumer(Consumer):
             with app.app_context():
                 if not result:
                     print('New Call')
+                    log.info('--- New Call ---')
                     result = await call.answer()
                     if result.successful:
                         call_id = result.event.payload.get('call_id')
                         from_phone = result.event.payload.get('device').get('params').get('from_number')
-                        
                                                     
-                        answer = f'Hello, welcome to the Emergercy Hotline! How may I assist you today?'
+                        answer = f'ECC, What is your Emergency?'
+
+                        log.info(f'from phone: {from_phone} \nquestion: {question} \nanswer: {answer}')
                         Call.create(from_phone, call_id, question, answer)
                         question = await call.prompt_tts(prompt_type='speech', text=answer)
                         await self.on_incoming_call(call, question.result, result)
                 else:
                     print('Continued Call')
+                    log.info('--- Continue Call ---')
                     call_id = result.event.payload.get('call_id')
                     from_phone = result.event.payload.get('device').get('params').get('from_number')
 
@@ -37,6 +40,8 @@ class CustomConsumer(Consumer):
 
                     history = Call.get_by_session_id(call_id)
                     answer = qa_chain(question, history)
+                    
+                    log.info(f'from phone: {from_phone} \nquestion: {question} \nanswer: {answer}')
                     Call.create(from_phone, call_id, question, answer)
                     question = await call.prompt_tts(prompt_type='speech', text=answer)
                     await self.on_incoming_call(call, question.result, result)
