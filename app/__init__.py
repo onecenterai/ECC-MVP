de308b60b7d4c7238de281e73c32082472168219
Gift-py <giftabumere247@gmail.com>
2024-05-22 14:09:30 +0100

    .

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
+
 import os
 from dotenv import load_dotenv
 from config.db import Base
@@ -17,7 +17,6 @@ app.config.setdefault('SQLALCHEMY_DATABASE_URI', os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
-JWTManager(app)
 
 # print('checking metadata ...')
 # print('Base Metadata: ', Base.metadata)
