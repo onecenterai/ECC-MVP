1b865d1f302a926120b184f063b32a655394455e
Gift-py <giftabumere247@gmail.com>
2024-05-23 18:17:13 +0100

    .

--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ log = logging.getLogger(__name__)
 info_handler = logging.FileHandler('/sw_log.log')
 info_handler.setLevel(logging.INFO)
 
-info_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-info_handler.setFormatter(info_format)
+# info_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# info_handler.setFormatter(info_format)
 
 log.addHandler(info_handler)
