52167f70c15636d642d6ad6262f756a2b2095760
Gift-py <giftabumere247@gmail.com>
2024-08-14 09:33:34 +0100

    Deployment to test monitor server v3

--- app/call/controller.py
+++ app/call/controller.py
@@ -20,7 +20,7 @@ load_dotenv()
 
 bp = Blueprint('call', __name__, template_folder='templates')
 
-whisper_model = whisper.load_model('base')
+#whisper_model = whisper.load_model('base')
 SILENCE_THRESHOLD = os.getenv('SILENCE_THRESHOLD')
 SILENCE_DURATION = os.getenv('SILENCE_DURATION')
 
@@ -222,101 +222,101 @@ def forward_call(answer):
 #     except Exception as e:
 #          raise e
 
-@sock.route('/stream')
-def stream(ws):
-    global i
-    global transcription0
-    try:
-        audio_buffer = b''
+# @sock.route('/stream')
+# def stream(ws):
+#     global i
+#     global transcription0
+#     try:
+#         audio_buffer = b''
         
-        while True:
-            message = ws.receive()
-            packet = json.loads(message)
-            if packet['event'] == 'start':
-                print('Starting Stream')
-            elif packet['event'] == 'stop':
-                print('Stopping Stream')
-                save_audio_wf(audio_buffer)
+#         while True:
+#             message = ws.receive()
+#             packet = json.loads(message)
+#             if packet['event'] == 'start':
+#                 print('Starting Stream')
+#             elif packet['event'] == 'stop':
+#                 print('Stopping Stream')
+#                 save_audio_wf(audio_buffer)
                 
-                # Convert in-ram buffer to something the model can use directly without needing a temp file.
-                # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
-                # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
-                audio_np = np.frombuffer(audio_buffer, dtype=np.int64).astype(np.float32) / 32768.0
+#                 # Convert in-ram buffer to something the model can use directly without needing a temp file.
+#                 # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
+#                 # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
+#                 audio_np = np.frombuffer(audio_buffer, dtype=np.int64).astype(np.float32) / 32768.0
 
-                transcription = whisper_model.transcribe(audio_np, fp16=torch.cuda.is_available())
-                question = transcription['text'].strip()
-                print(f'\nWHISPER TRANSCRIPTION: {question}\n')
-                audio_buffer = b''
-            elif packet['event'] == 'media':
-                i += 1
-                audio = base64.b64decode(packet['media']['payload'])
-                audio = audioop.ulaw2lin(audio, 2)
-                audio = audioop.ratecv(audio, 2, 1, 8000, 16000, None)[0]
-                audio_buffer += audio
+#                 transcription = whisper_model.transcribe(audio_np, fp16=torch.cuda.is_available())
+#                 question = transcription['text'].strip()
+#                 print(f'\nWHISPER TRANSCRIPTION: {question}\n')
+#                 audio_buffer = b''
+#             elif packet['event'] == 'media':
+#                 i += 1
+#                 audio = base64.b64decode(packet['media']['payload'])
+#                 audio = audioop.ulaw2lin(audio, 2)
+#                 audio = audioop.ratecv(audio, 2, 1, 8000, 16000, None)[0]
+#                 audio_buffer += audio
 
-                if len(audio_buffer) > 299999:
-                    audio_file = 'transcription'+str(i)+'.wav'
-                    save_audio_wf(audio_data=audio_buffer, filename=audio_file)
-                    audio_file = open(audio_file, 'rb')
+#                 if len(audio_buffer) > 299999:
+#                     audio_file = 'transcription'+str(i)+'.wav'
+#                     save_audio_wf(audio_data=audio_buffer, filename=audio_file)
+#                     audio_file = open(audio_file, 'rb')
 
 
-                # Convert in-ram buffer to something the model can use directly without needing a temp file.
-                # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
-                # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
-                # audio_np = np.frombuffer(audio_buffer, dtype=np.int64).astype(np.float32) / 32768.0
-                try:
-                    transcription = whisper_model.transcribe(audio_file)
-                    if (transcription != transcription0):
-                        print(transcription['text'] + ' ')
-                        transcription0 = transcription
-                    audio_file.close()
-                except:
-                    print('An error occured while transcribing')
-                    pass
+#                 # Convert in-ram buffer to something the model can use directly without needing a temp file.
+#                 # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
+#                 # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
+#                 # audio_np = np.frombuffer(audio_buffer, dtype=np.int64).astype(np.float32) / 32768.0
+#                 try:
+#                     transcription = whisper_model.transcribe(audio_file)
+#                     if (transcription != transcription0):
+#                         print(transcription['text'] + ' ')
+#                         transcription0 = transcription
+#                     audio_file.close()
+#                 except:
+#                     print('An error occured while transcribing')
+#                     pass
 
                 
-                audio_buffer = b''
+#                 audio_buffer = b''
                 
-                '''
-                    NOTE !!
-                    DETECT SILENCE AND SEND MESSAGE TO LLM MODEL
-                    STILL UNDER WORK SHA
-                '''
-                # # Append to the main audio buffer
-                # audio_buffer += audio
+#                 '''
+#                     NOTE !!
+#                     DETECT SILENCE AND SEND MESSAGE TO LLM MODEL
+#                     STILL UNDER WORK SHA
+#                 '''
+#                 # # Append to the main audio buffer
+#                 # audio_buffer += audio
                 
-                # # Check for silence
-                # rms = audioop.rms(audio, 2)
-                # if rms < SILENCE_THRESHOLD:
-                #     if silence_start is None:
-                #         silence_start = time.time()
-                #     silence_buffer += audio
-                # else:
-                #     silence_start = None
-                #     silence_buffer = b''
+#                 # # Check for silence
+#                 # rms = audioop.rms(audio, 2)
+#                 # if rms < SILENCE_THRESHOLD:
+#                 #     if silence_start is None:
+#                 #         silence_start = time.time()
+#                 #     silence_buffer += audio
+#                 # else:
+#                 #     silence_start = None
+#                 #     silence_buffer = b''
                 
-                # if silence_start and (time.time() - silence_start) >= SILENCE_DURATION:
-                #     print('Silence Detected, stopping stream.')
-                #     save_audio_wf(audio_buffer)
-                #     audio_buffer = b''
-                #     break  # Exit the loop if silence is detected for a duration # Append to the main audio buffer
-                # audio_buffer += audio
+#                 # if silence_start and (time.time() - silence_start) >= SILENCE_DURATION:
+#                 #     print('Silence Detected, stopping stream.')
+#                 #     save_audio_wf(audio_buffer)
+#                 #     audio_buffer = b''
+#                 #     break  # Exit the loop if silence is detected for a duration # Append to the main audio buffer
+#                 # audio_buffer += audio
                 
-                # # Check for silence
-                # rms = audioop.rms(audio, 2)
-                # if rms < SILENCE_THRESHOLD:
-                #     if silence_start is None:
-                #         silence_start = time.time()
-                #     silence_buffer += audio
-                # else:
-                #     silence_start = None
-                #     silence_buffer = b''
+#                 # # Check for silence
+#                 # rms = audioop.rms(audio, 2)
+#                 # if rms < SILENCE_THRESHOLD:
+#                 #     if silence_start is None:
+#                 #         silence_start = time.time()
+#                 #     silence_buffer += audio
+#                 # else:
+#                 #     silence_start = None
+#                 #     silence_buffer = b''
 
-                # if silence_start and (time.time() - silence_start) >= SILENCE_DURATION:
-                #     print('Silence Detected, stopping stream.')
-                #     save_audio_wf(audio_buffer)
-                #     audio_buffer = b''
-                #     break  # Exit the loop if silence is detected for a duration
+#                 # if silence_start and (time.time() - silence_start) >= SILENCE_DURATION:
+#                 #     print('Silence Detected, stopping stream.')
+#                 #     save_audio_wf(audio_buffer)
+#                 #     audio_buffer = b''
+#                 #     break  # Exit the loop if silence is detected for a duration
 
-    except Exception as e:
-        raise e
+#     except Exception as e:
+#         raise e
